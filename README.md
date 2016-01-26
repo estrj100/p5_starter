1369d6da165363532ceb241070535a33630b37d0
Alison Abreu-Garcia <alison.iag@gmail.com>
2016-01-25 21:58:41 -0500

    set up p5 template

---
+++ README.md
@@ -0,0 +1,3 @@
+## p5 project template
+
+This is a basic setup for a p5 project. You can edit `sketch.js` and view your project by opening `index.html` in the browser. 
---
+++ sketch.js
@@ -0,0 +1,11 @@
+//This is a starter kit for p5 projects
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+  background("cadetblue");
+
+  ellipse(windowWidth/2,windowHeight/2,100,100);
+}
